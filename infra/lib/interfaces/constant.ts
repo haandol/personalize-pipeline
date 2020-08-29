5e5899b8e71bcb7c5b03945db84cbbaac07ada05
Dongkyun Lee <dongkyl@amazon.com>
2020-08-29 13:33:48 +0900

    add license to each file

--- infra/lib/interfaces/constant.ts
+++ infra/lib/interfaces/constant.ts
@@ -1,4 +1,21 @@
-/* TODO: replace vpc-id with yours for Private ApiGateway
+/* *****************************************************************************
+ * * Copyright 2019 Amazon.com, Inc. and its affiliates. All Rights Reserved.  *
+ *                                                                             *
+ * Licensed under the Amazon Software License (the "License").                 *
+ *  You may not use this file except in compliance with the License.           *
+ * A copy of the License is located at                                         *
+ *                                                                             *
+ *  http://aws.amazon.com/asl/                                                 *
+ *                                                                             *
+ *  or in the "license" file accompanying this file. This file is distributed  *
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either  *
+ *  express or implied. See the License for the specific language governing    *
+ *  permissions and limitations under the License.                             *
+ * *************************************************************************** *
+*/
+
+/*
+ * TODO: replace vpc-id with yours for Private ApiGateway
  *
  * @vpcId                 id of VPC
  * @vpceId                id of VPC Endpoint for API Gateway(execute-api)
@@ -13,7 +30,8 @@ export const VpcProps = {
   availabilityZones: ['ap-northeast-2a', 'ap-northeast-2b', 'ap-northeast-2c'],
 }
 
-/* TODO: replace account and region with yours
+/*
+ * TODO: replace account and region with yours
  * 
  * @account   account id, e.g. 929831892372
  * @region    region of VPC, e.g. ap-northeast-2
@@ -27,7 +45,8 @@ export const StackProps = {
 
 export const ns = 'PersonalizeAlpha';
 
-/* TODO: replace email and slack notifycation address
+/*
+ * TODO: replace email and slack notifycation address
  *
  * @notifySender    // sender's email address, e.g. DongGyun Lee <dongkyl@amazon.com>
  * @notifyEmail     // receiver's email address
